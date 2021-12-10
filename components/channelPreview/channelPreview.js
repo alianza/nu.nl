9965d66827bccc24547ebb931321da0b40786a25
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-12-10 03:26:10 +0100

    Added stories component

--- components/channelPreview/channelPreview.js
+++ components/channelPreview/channelPreview.js
@@ -1,21 +1,22 @@
-import { formatDate } from "../../lib/formatDate"
+import Link from 'next/link'
+import Story from "../story/story"
 
 export default function ChannelPreview({channel}) {
     return (
         <div className="flex flex-col gap-4">
             <div className="flex flex-col items-center">
-                <h1 className="text-xl">{channel.title}</h1>
+                <Link href={channel.link.substr(channel.link.lastIndexOf('/'), channel.link.length)}><a>
+                    <h1 className="text-2xl">{channel.title}</h1>
+                </a></Link>
                 <span
-                    className="text-accent-6">Laatste data: {new Date(channel.lastBuildDate).toLocaleTimeString('nl')}</span>
+                    className="text-accent-6">
+                    Laatste data: {new Date(channel.lastBuildDate).toLocaleTimeString('nl')}
+                </span>
             </div>
 
-            <ul className="flex flex-wrap justify-center gap-4 w-full">
+            <ul className="flex flex-wrap justify-center gap-8 tablet:gap-4 w-full">
                 {channel.item.map((item, index) => (
-                    <li key={index} className="flex flex-col w-64 gap-2">
-                        <a href={item.link}><h2 className="text-xl">{item.title}</h2></a>
-                        <span>{formatDate(new Date(item.pubDate))} om: {new Date(item.pubDate).toLocaleTimeString('nl', {hour12: false})}</span>
-                        <p className="text-sm" dangerouslySetInnerHTML={{__html: item.description}}/>
-                    </li>
+                    <Story key={index} item={item}/>
                 ))}
             </ul>
         </div>
