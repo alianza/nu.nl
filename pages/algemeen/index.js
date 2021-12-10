bc4b6584cf1bea15b9901d6eece76ca8262f8a25
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-12-10 03:28:57 +0100

    Added algemeen full channel page

---
+++ pages/algemeen/index.js
@@ -0,0 +1,27 @@
+import { NuService } from "../../lib/services/nuService"
+import ChannelPreview from "../../components/channelPreview/channelPreview"
+
+export async function getStaticProps() {
+    const algemeen = await NuService.getAlgemeen()
+
+    return {
+        props: {
+            channels: [
+                algemeen
+            ],
+            buildTime: new Date().toString()
+        },
+        revalidate: 120
+    }
+}
+
+export default function Home({ channels,  buildTime }) {
+
+    console.log('buildTime:' , new Date(buildTime).toLocaleString('nl'))
+
+    return (
+        <div className="flex flex-col items-center gap-8">
+            {channels.map(channel => ( <ChannelPreview key={channel.title} channel={channel} /> ))}
+        </div>
+    )
+}
