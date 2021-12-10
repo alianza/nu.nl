5308a2d6a6cc150fbdd57c172ce71cc0b6c11869
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-12-10 03:28:08 +0100

    Improved loader, updated styles

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import 'tailwindcss/tailwind.css'
 import '../styles/global.scss'
 import Layout from "../components/layout/layout";
 import NextNProgress from "nextjs-progressbar";
+import { useDarkThemeListener } from "../lib/eventListeners"
+import { useState } from "react"
+import Head from "next/head"
 
 export async function getStaticProps() {
     return {
@@ -13,10 +16,17 @@ export async function getStaticProps() {
 }
 
 function MyApp({ Component, pageProps }) {
+    const [darkTheme, setDarkTheme] = useState(false)
+
+    useDarkThemeListener(setDarkTheme)
+
   return(
     <Layout buildTime={pageProps.buildTime}>
+        <Head>
+            <meta name="theme-color" content={darkTheme ? '#000' : '#fff'}/>
+        </Head>
         <Component {...pageProps} />
-        <NextNProgress color={'#020051'}/>
+        <NextNProgress color={darkTheme ? '#fff' : '#000'}/>
     </Layout>
   )
 }
