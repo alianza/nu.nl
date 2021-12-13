0c34cd75cf3cd4bd0dd036f5b57fb806a2ee23bf
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-12-13 12:42:24 +0100

    Upgrade Tailwindcss to v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
@@ -32,9 +30,6 @@ module.exports = {
         'footer': 'var(--footer)',
       },
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
