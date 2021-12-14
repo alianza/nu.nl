dfaed5166cb29a1ff1c1e6f7b849f0a2bb69625d
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-12-14 14:56:24 +0100

    Fixed footer buildDate

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,7 +1,12 @@
-export default function Footer(props) {
-    return <footer className="absolute w-full bottom-0 h-16 p-4 shadow-allround flex justify-between items-center">
+import { formatDate } from "../../lib/formatDate"
+
+export default function Footer({buildTime}) {
+    const buildTimeDate = new Date(buildTime)
+
+    return (
+        <footer className="absolute w-full bottom-0 h-16 p-4 shadow-allround flex justify-between items-center">
             <span>Author: <a href="https://jwvbremen.nl" target="_blank"
                              rel="noreferrer">Jan-Willem van Bremen</a></span>
-        <span>{new Date(props.buildTime).toLocaleString("nl")}</span>
-    </footer>
+        <span>Pagina gegenereerd: <span className="italic">{formatDate(buildTimeDate)} om: {("0" + buildTimeDate.getHours()).slice(-2)}:{("0" + buildTimeDate.getMinutes()).slice(-2)}</span></span>
+    </footer>)
 }
