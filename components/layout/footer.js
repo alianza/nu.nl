9f38a09b6af6f76e424d22880161ff371dd4ffb3
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2021-12-15 11:16:06 +0100

    Changed header and footer style

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -5,7 +5,7 @@ export default function Footer({buildTime}) {
     const buildTimeDate = new Date(buildTime)
 
     return (
-        <footer className="absolute w-full bottom-0 h-16 p-4 shadow-allround flex justify-between items-center">
+        <footer className="absolute w-full bottom-0 h-16 p-4 shadow-allround bg-accent-1 flex justify-between items-center">
             <span>Author: <a href="https://jwvbremen.nl" target="_blank"
                              rel="noreferrer">Jan-Willem van Bremen</a></span>
         <span>Pagina gegenereerd: <span className="italic">{formatDate(buildTimeDate)} om: {formatTime(buildTimeDate)}</span></span>
