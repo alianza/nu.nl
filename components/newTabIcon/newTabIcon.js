8f71ab8ee7bb986c64450672002388f4303ccc66
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-01-31 17:08:48 +0100

    Fix secure links and open in new tab icon alt text

--- components/newTabIcon/newTabIcon.js
+++ components/newTabIcon/newTabIcon.js
@@ -1,4 +1,3 @@
 export default function NewTabIcon({className}) {
-    return (<img className={className}
-                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg=="/>)
+    return (<img className={className} alt="Open story in new tab" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAQElEQVR42qXKwQkAIAxDUUdxtO6/RBQkQZvSi8I/pL4BoGw/XPkh4XigPmsUgh0626AjRsgxHTkUThsG2T/sIlzdTsp52kSS1wAAAABJRU5ErkJggg=="/>)
 }
