6ca1468411e8c78eba5e8fd6fc17f7854505d0d6
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-22 18:24:40 +0100

    Cleanup

--- pages/tech/index.js
+++ pages/tech/index.js
@@ -1,35 +1,35 @@
-import { NuService } from "../../lib/services/nuService"
-import Channel from "../../components/channelPreview/channel"
-import { useState } from "react"
-import StoryDialog from "../../components/storyDialog/storyDialog"
-import Head from "next/head"
+import { NuService } from "../../lib/services/nuService";
+import Channel from "../../components/channelPreview/channel";
+import { useState } from "react";
+import StoryDialog from "../../components/storyDialog/storyDialog";
+import Head from "next/head";
 
 export async function getStaticProps() {
-    const tech = await NuService.getTech()
+  const tech = await NuService.getTech();
 
-    return {
-        props: {
-            channels: [
-                tech
-            ],
-            buildTime: new Date().toString()
-        },
-        revalidate: 60
-    }
+  return {
+    props: {
+      channels: [tech],
+      buildTime: new Date().toString(),
+    },
+    revalidate: 60,
+  };
 }
 
 export default function Home({ channels }) {
-    const [story, setStory] = useState(null)
+  const [story, setStory] = useState(null);
 
-    return (
-        <div className="flex flex-col items-center gap-8">
-            <Head>
-                <title>Nu.nl Feeds - {channels[0].title}</title>
-            </Head>
+  return (
+    <div className="flex flex-col items-center gap-8">
+      <Head>
+        <title>Nu.nl Feeds - {channels[0].title}</title>
+      </Head>
 
-            {channels.map(channel => ( <Channel key={channel.title} openStory={setStory} channel={channel}/> ))}
+      {channels.map((channel) => (
+        <Channel key={channel.title} openStory={setStory} channel={channel} />
+      ))}
 
-            <StoryDialog story={story} setStory={setStory}/>
-        </div>
-    )
+      <StoryDialog story={story} setStory={setStory} />
+    </div>
+  );
 }
